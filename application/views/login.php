b861ffb69b695f57fed0042127cacfc2aaf59026
Sonam Serchan <sonamserchan@Sonams-MacBook-Pro.local>
2018-05-16 21:03:41 +1000

    testing the first commit. Just centered the login panel. s	sdkjkl psΩ≈ esdsasanter the commit message for your changes. Lines starting

--- application/views/login.php
+++ application/views/login.php
@@ -32,7 +32,7 @@
 
 	<div class="container">
 	    <div class="row">
-	        <div class="col-md-4"><!--<div class="col-md-4 col-md-offset-4">-->
+	        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-4" style="margin: auto 30%"><!--<div class="col-md-4 col-md-offset-4">-->
 	            <div class="login-panel panel panel-default">
 	                <div class="panel-heading">
 	                    <h3 class="panel-title"> Diabetes Control Australia - Please Sign In</h3>
