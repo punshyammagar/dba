9ce045a61c6119f29bee16bcb0e27a3a2fff37bf
punshyammagar <punshyammagar@gmail.com>
2018-05-30 20:59:07 +1000

    added insert for exercise plan

--- application/models/Exercise_plan_model.php
+++ application/models/Exercise_plan_model.php
@@ -9,6 +9,37 @@ class exercise_plan_model extends CI_Model {
 		parent::__construct();
 	}
 
+	function insert_exercise_plan($postData){
+       $validate = $this->validate_customer_id($postData);
+
+        if($validate){
+            $data = array(
+                'customer_id' => $postData['customerid'],
+                'exercise_name' => $postData['exercise_name'],
+                'frequency' =>$postData['frequency'],
+                'start_date' => $postData['sd'],
+                'end_date' =>$postData['ed'],
+                'description' => $postData['description'],
+            );
+            $this->db->insert('exercise_plan', $data);
+            return array('status' => 'success', 'message' => '');
+
+        }else{
+            return array('status' => 'exist', 'message' => '');
+        }
+    }
+
+    function validate_customer_id($postData){
+      $this->db->where('customer_id', $postData['customerid']);
+      $this->db->from('customer');
+      $query=$this->db->get();
+
+      if ($query->num_rows() == 1)
+        return true;
+      else
+        return false;
+    }
+
 	function get_exercise_plan_data(){
 		$this->db->select('*');
 		$this->db->from('customer');
