ccef0c00bb377af98783c1411101acf46c4d6ed6
punshyammagar <punshyammagar@gmail.com>
2018-05-30 19:44:00 +1000

    added insert in model for nutrition plan

--- application/models/nutrition_plan_model.php
+++ application/models/nutrition_plan_model.php
@@ -9,6 +9,37 @@ class nutrition_plan_model extends CI_Model {
 		parent::__construct();
 	}
 
+	function insert_nutrition_plan($postData){
+       $validate = $this->validate_customer_id($postData);
+
+        if($validate){
+            $data = array(
+                'customer_id' => $postData['customerid'],
+                'food' => $postData['food'],
+                'amount' =>$postData['amount'],
+                'start_date' => $postData['sd'],
+                'end_date' =>$postData['ed'],
+                'description' => $postData['description'],
+            );
+            $this->db->insert('nutrition_plan', $data);
+            return array('status' => 'success', 'message' => '');
+
+        }else{
+            return array('status' => 'exist', 'message' => '');
+        }
+    }
+
+    function validate_customer_id($postData){
+      $this->db->where('customer_id', $postData['customerid']);
+      $this->db->from('customer');
+      $query=$this->db->get();
+
+      if ($query->num_rows() == 1)
+        return true;
+      else
+        return false;
+    }
+
 	function get_nutrition_plan_data(){
 		$this->db->select('*');
 		$this->db->from('customer');
