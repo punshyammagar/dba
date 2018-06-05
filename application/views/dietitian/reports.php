5bd3076e5479b9641bf7ad4126dc9a9704b2202d
punshyammagar <punshyammagar@gmail.com>
2018-06-05 18:23:20 +1000

    added dummy chart for report page

--- application/views/dietitian/reports.php
+++ application/views/dietitian/reports.php
@@ -1,37 +1,59 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<body>
+<div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="page-header"><?=$title?></h3>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
 
-<h1>My Web Page</h1>
+            <?php if($this->session->flashdata('success')):?>
+                <div class="alert alert-success">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                <strong><?php echo $this->session->flashdata('success'); ?></strong>
+                </div>
+            <?php elseif($this->session->flashdata('error')):?>
+                <div class="alert alert-warning">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                <strong><?php echo $this->session->flashdata('error'); ?></strong>
+                </div>
+            <?php endif;?>
+            <div class="row">
+                <div class="col-lg-12">      
+                    <div id="piechart" class="row"></div>
 
-<div id="piechart" class="row"></div>
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <script type="text/javascript">
+                      // Load google charts
+                      google.charts.load('current', {'packages':['corechart']});
+                      google.charts.setOnLoadCallback(drawChart);
 
-<script type="text/javascript">
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+                      // Draw the chart and set the chart values
+                      function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                          ['Task', 'Hours per Day'],
+                          ['Work', 8],
+                          ['Eat', 2],
+                          ['TV', 4],
+                          ['Gym', 2],
+                          ['Sleep', 8]
+                        ]);
 
-// Draw the chart and set the chart values
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['Task', 'Hours per Day'],
-  ['Work', 8],
-  ['Eat', 2],
-  ['TV', 4],
-  ['Gym', 2],
-  ['Sleep', 8]
-]);
+                        // Optional; add a title and set the width and height of the chart
+                        var options = {'title':'My Average Day', 'width':550, 'height':400};
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'My Average Day', 'width':550, 'height':400};
+                        // Display the chart inside the <div> element with id="piechart"
+                        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                        chart.draw(data, options);
+                      }
+                    </script>
 
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
-</script>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
 
-</body>
-</html>
+
+<?php $this->load->view('template/footer_view')?>
