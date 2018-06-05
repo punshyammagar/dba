29f2a9d31a693a974ea2598c6519d2da8189877d
punshyammagar <punshyammagar@gmail.com>
2018-06-05 18:22:47 +1000

    added charts for admin dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -16,14 +16,102 @@
                     <h3 class="page-header">Dashboard</h3>
                 </div>
                 <br>
-                <div class="row">
-                    <div class="col-lg-4 col-md-4">
-                        <a href="<?=base_url('admin/user_list')?>/" class="btn btn-lilac btn-lg" role="button"><i class="fa fa-user glyphsize"></i> <br/>User List</a>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <a href="<?=base_url('admin/activity_log')?>/" class="btn btn-lilac btn-lg" role="button"><i class="glyphicon glyphicon-user glyphsize"></i> <br/>Activity Log</a>
-                    </div>
+
+                <div class="col-lg-6">
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <div id="donutchart"></div>
+                    <script type="text/javascript">
+                      google.charts.load("current", {packages:["corechart"]});
+                      google.charts.setOnLoadCallback(drawChart);
+                      function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                          ['User Group', 'No of User'],
+                          ['Administrator', 8],
+                          ['Dietitian', 4],
+                        ]);
+
+
+                        var options = {
+                          title: 'User Group',
+                          pieHole: 0.4,
+                          backgroundColor: '#f1f8e9',
+                                width:550,
+                                height:400
+                        };
+
+                        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+                        chart.draw(data, options);
+                      }
+                    </script>
+                 </div>    
+
+                <!-- <div class="col-lg-6">
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <div id="piechart"></div>
+                    <script type="text/javascript">
+                      // Load google charts
+                      google.charts.load('current', {'packages':['corechart']});
+                      google.charts.setOnLoadCallback(drawChart);
+
+                      // Draw the chart and set the chart values
+                      function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                          ['User Group', 'No of User'],
+                          ['Administrator', 8],
+                          ['Dietitian', 4],
+                        ]);
+
+                        // Optional; add a title and set the width and height of the chart
+                        var options = {'title':'User Group',backgroundColor: '#f1f8e9', 'width':550, 'height':400};
+
+                        // Display the chart inside the <div> element with id="piechart"
+                        var chart = new google.visualization.BarChart(document.getElementById('piechart'));
+                        chart.draw(data, options);
+                      }
+                    </script>
+                </div> -->
+                <div class="col-lg-6">
+                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                    <div id="chart_div"></div>
+
+                    <script type="text/javascript">
+                        google.charts.load('current', {packages: ['corechart', 'line']});
+                        google.charts.setOnLoadCallback(drawBackgroundColor);
+
+                        function drawBackgroundColor() {
+                              var data = new google.visualization.DataTable();
+                              data.addColumn('number', 'X');
+                              data.addColumn('number', 'Activity');
+
+                              data.addRows([
+                                [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+                                [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+                                [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+                                [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+                                [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+                                [30, 55]
+                              ]);
+
+                              var options = {
+                                hAxis: {
+                                  title: 'Day'
+                                },
+                                vAxis: {
+                                  title: 'Activity'
+                                },
+                                backgroundColor: '#f1f8e9',
+                                width:550,
+                                height:400
+                              };
+
+                              var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+                              chart.draw(data, options);
+                            }
+                    </script>
+
+
                 </div>
+
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
